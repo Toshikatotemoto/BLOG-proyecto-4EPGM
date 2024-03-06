eec4b2f178c0df52d1bbfc9c986c35d8abaac34d
Toshikatotemoto <161533448+Toshikatotemoto@users.noreply.github.com>
2024-03-05 18:19:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ $alimento1 = [
 $alimento2 = [
    "Nombre" => "Pizza"
    "Calorias" => 266
-   "Precio" => 25
+   ];
+
+$alimento3= [
+   "Nombre" => "Pollo enchildo con arroz"
+   "Calorias" => 489
+   "Precio" => 276 
    ];
 
 
